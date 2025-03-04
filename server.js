ee5152cf12e2a015503df26ae1e2168bf8e429cf
Steven <stev508ga@gmail.com>
2025-03-03 21:35:42 -0500

    Corrección

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('*', (req, res) => {
 
 // Expose
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
