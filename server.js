1223cad6ec041abf57a52439188f9aa167d9ca11
Steven <stev508ga@gmail.com>
2025-03-26 21:35:03 -0500

    puerto

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
 });
 
 // Expose
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
